1f7717a8e7aee29d6f8079fb77103d6bfa4ceac8
John Haynes <john.andrew.haynes@gmail.com>
2020-08-02 09:29:13 +0200

    add docker build and run scripts

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Build the indexer/query processor
+docker build indexer-qp2 --tag indexer-qp2
+
+# Build the api
+docker build searchapi3 --tag searchapi3
+
+# Build the crawler
+# docker build crawler3 --tag crawler3
+
+# Build the deployment
+docker build deploy --tag deploy
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -it --name deploy -p 8081:8081 deploy
