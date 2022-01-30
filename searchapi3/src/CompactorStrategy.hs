8fec50461e5399dd6174c1d6c561adcdebd14f9c
John Haynes <john.andrew.haynes@gmail.com>
2022-01-30 11:25:19 +0100

    allow the fibonacci compactor to recover non-fibonacci components

--- searchapi3/src/CompactorStrategy.hs
+++ searchapi3/src/CompactorStrategy.hs
@@ -1,13 +1,13 @@
-module CompactorStrategy where
+module CompactorStrategy ( hybridStrategy ) where
 
 import           Component     ( Component )
-import           Registry      ( Registry (..) )
-import           Types         ( CollectionName, numDocs )
+import           Types         ( numDocs )
 
-import           Control.Concurrent.STM      (STM)
 import           Data.IntSet                 (IntSet)
 import qualified Data.IntSet           as IS
-import           Data.List                   (groupBy)
+import           Data.List                   (maximumBy, sortBy)
+import           Data.Ord                    (comparing)
+import           Data.Set                    (Set)
 import qualified Data.Set              as S
 
 fibSet :: IntSet
@@ -15,20 +15,67 @@ fibSet = IS.fromAscList $! take 100 fibs
     where
     fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
-{- TODO!  need to recover from non-fibonacci cases
-   e.g. when merges overlap -}
-largestFibonacciStrategy :: Registry -> CollectionName -> STM (Maybe [Component])
-largestFibonacciStrategy registry collectionName = do
-    components <- S.toDescList <$> viewCollectionComponents registry collectionName
-    let candidates = map (take 2)
-                   . filter (\xs -> length xs > 1)
-                   $ groupBy sumToFibonacci components
-    case candidates of
-        (pair@[x,y]:_) -> do takeLock registry x     -- Not very RAII FIXME
-                             takeLock registry y
-                             pure $ Just pair
-        _              -> pure Nothing
+hybridStrategy :: Set Component
+               -> Maybe (String, Component, Component)
+hybridStrategy componentSet =
+    let components = sortBy (comparing numDocs) $ S.toList componentSet
+        nonFibComponents = filter (not . isFib) components
+    in if null nonFibComponents
+        then largestFibonacciStrategy' components
+        else nonFibonacciStrategy components nonFibComponents
 
     where
-    sumToFibonacci :: Component -> Component -> Bool
-    sumToFibonacci a b = (numDocs b + numDocs a) `IS.member` fibSet
+    isFib :: Component -> Bool
+    isFib cmp = numDocs cmp `IS.member` fibSet
+
+nonFibonacciStrategy :: [Component]
+                     -> [Component]
+                     -> (Maybe ([Char], Component, Component))
+nonFibonacciStrategy components nonFibComponents =
+    -- Find gap
+            let maxNonFib    = maximum nonFibComponents
+                Just nextFib = IS.lookupGT (numDocs maxNonFib) fibSet
+                gap          = nextFib - numDocs maxNonFib
+                fillers      = filter (\c -> numDocs c == gap)
+                             $ filter (\c -> c /= maxNonFib) components
+
+            -- Can we fill that gap with one component?
+            in case fillers of
+
+                -- Yes
+                (f:_) -> Just ("gap 1", f, maxNonFib)
+
+                -- No
+                _ -> incrementalGapFill components maxNonFib gap
+
+incrementalGapFill :: [Component]
+                   -> Component
+                   -> Int
+                   -> (Maybe (String, Component, Component))
+incrementalGapFill components maxNonFib gap =
+    let otherComponents = filter (\c -> c /= maxNonFib) components
+    -- Algorithm choice: find the best adjacent (O(n)) pair, or best non-adjacent (O(n2)) pair?
+    -- Trying adjacent pairs for now
+        candidatePairs = filter (\(a,b) -> numDocs a + numDocs b <= gap)
+                       $ pairs otherComponents
+    in if null candidatePairs
+           then Nothing
+           else let (p, q) = maximumBy (comparing (\(a,b) -> numDocs a + numDocs b)) candidatePairs
+                in Just ("incrementalGapFill", p, q)
+
+largestFibonacciStrategy' :: [Component] -> (Maybe (String, Component, Component))
+largestFibonacciStrategy' components = do
+    let candidates = map (\(a,b) -> [a,b])
+                   . sortBy (flip $ comparing (\(a, b) -> numDocs a + numDocs b))
+                   . filter (\(a, b) -> (numDocs b + numDocs a) `IS.member` fibSet)
+                   $ pairs components  -- groupby was wrong here.
+    case candidates of
+        ([x,y]:_) -> Just ("fib", x, y)
+        _         -> Nothing
+
+pairs :: [a] -> [(a, a)]
+pairs     [] = []
+pairs (x:xs) = go [] x xs
+    where
+    go acc _     [] = reverse acc
+    go acc p (y:ys) = go ((p,y):acc) y ys
