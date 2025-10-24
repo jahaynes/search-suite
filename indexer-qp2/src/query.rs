7e44a7fedc0054184e0c8e9e6bad37cab6e22397
John Haynes <john.andrew.haynes@gmail.com>
2025-10-24 16:01:08 +0200

    remove weird 'skip' logic

--- indexer-qp2/src/query.rs
+++ indexer-qp2/src/query.rs
@@ -19,17 +19,9 @@ pub fn query(ir:           &IndexRead,
 
     let mut scored = Vec::new();
 
-    let skip =
-          match query_params.max_results {
-            None    => false,
-            Some(n) => n < 1
-          };
-
-    if !skip {
-        run_query(&mut scored,
-                  ir,
-                  &query_params);
-    }
+    run_query(&mut scored,
+              ir,
+              &query_params);
 
     QueryResult { num_results: scored.len()
                 , results:     scored
