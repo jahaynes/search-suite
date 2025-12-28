e0bcb37c06c3d7b5acae97ecbb501ebdbf29fcfb
John Haynes <john.andrew.haynes@gmail.com>
2025-12-28 18:19:47 +0100

    giving it good bones

--- search-lib/test/Main.hs
+++ search-lib/test/Main.hs
@@ -1,4 +1,18 @@
-module Main (main) where
+module Main ( main ) where
+
+import Search (Env (..), Search, runSearch, throw)
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+
+    putStrLn "correctResult"
+    print =<< runSearch correctResult Env
+
+    putStrLn "immediatelyBad"
+    print =<< runSearch exceptional Env
+
+correctResult :: Search [Int]
+correctResult = pure [1, 2, 3]
+
+exceptional :: Search [Int]
+exceptional = throw ["foo"]
