efdcedaf60678e48c89230b64917b257a6b02cc2
John Haynes <john.andrew.haynes@gmail.com>
2026-01-01 17:12:44 +0100

    resourcing warc files

--- search-libs/warc/Warc/WarcFileWriter.hs
+++ search-libs/warc/Warc/WarcFileWriter.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE LambdaCase
-           , OverloadedStrings
            , ScopedTypeVariables #-}
 
 module WarcFileWriter ( WarcFileWriter (..)
@@ -13,15 +12,17 @@ import           Data.Warc.Header
 import           Data.Warc.Key
 import           Data.Warc.Value
 import           Data.Warc.Parse
+import           File
 
+import           Control.Monad.IO.Class
 import           Control.Monad               (forM_)
+import           Control.Monad.Trans.Resource
 import qualified Data.ByteString      as BS
 import qualified Data.ByteString.Lazy as LBS
 import           Data.Either                 (rights)
 import           Data.Serialize              (encode)
 import           Data.Word                   (Word64)
-import           System.IO
-import           UnliftIO.Exception          (bracket)
+import           System.IO                   (hTell)
 
 data WarcFileWriter =
     WarcFileWriter { writeWarcFile                   :: !(FilePath -> FilePath -> [WarcEntry] -> IO ())
@@ -35,67 +36,36 @@ createWarcFileWriter =
                    }
 
 -- TODO compress?
--- TODO exceptions
 -- TODO check need sorted?
 writeWarcFileImpl :: FilePath -> FilePath -> [WarcEntry] -> IO ()
-writeWarcFileImpl destWarcFile destOffsets wes = do
-
-    let acquire = do offs <- openBinaryFile destOffsets WriteMode
-                     warc <- openBinaryFile destWarcFile WriteMode
-                     pure (offs, warc)
-
-    let release (offs, warc) = do hClose offs
-                                  hClose warc
-
-    bracket acquire release $ \(offs, warc) ->
-
-        forM_ wes $ \we -> do
-
+writeWarcFileImpl destWarcFile destOffsets wes = runResourceT $ do
+        offs <- openForWriting destOffsets
+        warc <- openForWriting destWarcFile
+        liftIO $ forM_ wes $ \we -> do
             -- Record in file.offs the warc entry's position 
             pos :: Word64 <- fromIntegral <$> hTell warc
             BS.hPut offs (encode pos)
-
             -- Write entry
             LBS.hPut warc (toLazyByteString we)
 
 -- TODO what is behaviour of exception during (partial) acquire?
 interleaveSortedWarcFilesAtPathImpl ::(String -> IO ()) -> FilePath -> FilePath -> FilePath -> IO ()
-interleaveSortedWarcFilesAtPathImpl tempLogger x y dest = do
-
-    let sourceWarcX  = x    <> "/file.warc"
-        sourceWarcY  = y    <> "/file.warc"
-        destWarcFile = dest <> "/file.warc"
-        destOffsets  = dest <> "/file.offs"
-
-    let acquire = do warcX <- openBinaryFile sourceWarcX ReadMode
-                     warcY <- openBinaryFile sourceWarcY ReadMode
-                     dOffs <- openBinaryFile destOffsets WriteMode
-                     dWarc <- openBinaryFile destWarcFile WriteMode
-                     pure (warcX, warcY, dOffs, dWarc)
-
-    let release (warcX, warcY, dOffs, dWarc) = do hClose warcX
-                                                  hClose warcY
-                                                  hClose dOffs
-                                                  hClose dWarc
-
-    bracket acquire release $ \(warcX, warcY, dOffs, dWarc) -> do
-
+interleaveSortedWarcFilesAtPathImpl tempLogger x y dest = runResourceT $ do
+    warcX <- openForReading (x <> "/file.warc")
+    warcY <- openForReading (y <> "/file.warc")
+    dOffs <- openForWriting (dest <> "/file.warc")
+    dWarc <- openForWriting (dest <> "/file.offs")
+    liftIO $ do
         entriesX <- rights . fromByteString <$> LBS.hGetContents warcX
         entriesY <- rights . fromByteString <$> LBS.hGetContents warcY
-
         forM_ (merge entriesX entriesY) $ \case
-
             Left e -> tempLogger e
-
             Right we -> do
-
                 -- Record in file.offs the warc entry's position 
                 pos :: Word64 <- fromIntegral <$> hTell dWarc
                 BS.hPut dOffs (encode pos)
-
                 -- Write entry
                 LBS.hPut dWarc (toLazyByteString we)
-
 
     where
     merge :: [WarcEntry] -> [WarcEntry] -> [Either String WarcEntry]
