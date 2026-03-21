8081f66ce215e0fe0a2c7420895200be6e951f05
John Haynes <john.andrew.haynes@gmail.com>
2026-03-21 14:47:47 +0100

    remove unused

--- indexer-qp2/src/ranking.rs
+++ indexer-qp2/src/ranking.rs
@@ -1,18 +1,10 @@
 use crate::index_reader::*;
-use crate::input::*;
 use crate::terms::*;
 use crate::types::*;
 
-use intmap::IntMap;
 use serde::Serialize;
 use std::cmp::Ordering;
-use std::cmp::Reverse;
-use std::collections::BinaryHeap;
-
-pub struct _Posting {
-    pub doc_id: u32,
-    pub term_freq: u32,
-}
+use std::collections::HashMap;
 
 #[derive(Clone, Debug, Serialize)]
 pub struct Scored {
@@ -47,8 +39,6 @@ impl Ord for Scored {
         my_score.cmp(&other_score)
     }
 }
-
-use std::collections::HashMap;
 
 pub fn rank_result_bm25(
     ir: &IndexRead,
@@ -88,89 +78,6 @@ pub fn rank_result_bm25(
             }
         }
     }
-}
-
-// TODO - unused?
-pub fn _rank_results(
-    out_scored: &mut Vec<Scored>,
-    ir: &IndexRead,
-    query_params: &QueryParams,
-    results: &Vec<(TermEntry, Vec<_Posting>)>,
-) -> () {
-    let DocOffsetsRead(doc_offsets) = ir.doc_offsets;
-
-    let num_docs = doc_offsets.len() as u64;
-    let avg_doc_len = ir.total_doc_len as u32 / num_docs as u32;
-
-    // Keyed on doc_id
-    let mut m: IntMap<u32, Scored> = IntMap::new();
-
-    // Step 1 - aggregate them all
-    for (term, postings) in results {
-        for p in postings {
-            // This is pretty inefficient (binary search of large file inside tight loop)
-            match find_doc(ir, DocId(p.doc_id)) {
-                None => panic!(
-                    "shouldn't happen (missing docid {} from {})",
-                    p.doc_id, query_params.base_path
-                ),
-
-                Some(doc) => {
-                    // 'remove' is used instead of 'get' because 'insert' (below) does not overwrite
-                    let old_score = m.remove(p.doc_id.into()).unwrap_or(Scored {
-                        uri: "".to_string(),
-                        score: 0.0,
-                        term_count: 0,
-                    });
-                    let new_score = idf(num_docs, term.doc_freq as f64)
-                        * bm25(avg_doc_len, doc.doc_len, p.term_freq);
-
-                    let Url(uri) = doc.url;
-                    let new_scored = Scored {
-                        uri: uri,
-                        score: old_score.score + new_score,
-                        term_count: old_score.term_count + 1,
-                    };
-
-                    // warn - insert does not seem to overwrite
-                    m.insert(p.doc_id.into(), new_scored);
-                }
-            }
-        }
-    }
-
-    match query_params.max_results {
-        // Step 2 - keep all docs
-        None => {
-            for v in m.values() {
-                out_scored.push(v.to_owned());
-            }
-        }
-
-        // Step 2 - keep only the top num_docs
-        Some(mr) => {
-            let mut min_score_heap: BinaryHeap<Reverse<&Scored>> = BinaryHeap::new();
-            let mut score_heap_sz: u32 = 0;
-            for scored in m.values() {
-                // If we don't have enough docs yet, any result is good enough
-                if score_heap_sz < mr {
-                    score_heap_sz = score_heap_sz + 1;
-                    min_score_heap.push(Reverse(&scored));
-                } else {
-                    // Too many results, start comparing!
-                    let &Reverse(min) = min_score_heap.peek().unwrap();
-                    if scored > min {
-                        min_score_heap.pop();
-                        min_score_heap.push(Reverse(&scored));
-                    }
-                }
-            }
-
-            for Reverse(v) in min_score_heap {
-                out_scored.push(v.to_owned());
-            }
-        }
-    };
 }
 
 fn bm25(av_doc_len: u32, doc_len: u32, term_freq: u32) -> f64 {
