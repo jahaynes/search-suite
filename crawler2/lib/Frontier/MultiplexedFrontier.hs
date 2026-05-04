91501dbee18f5c4daa32514b6903923bf8eb4a66
John Haynes <john.andrew.haynes@gmail.com>
2026-05-04 11:00:41 +0200

    mess

--- crawler2/lib/Frontier/MultiplexedFrontier.hs
+++ crawler2/lib/Frontier/MultiplexedFrontier.hs
@@ -3,17 +3,29 @@
 
 module Frontier.MultiplexedFrontier where
 
-import Frontier.Class
-import Frontier.PoliteStmFrontier -- Pass this in instead!
-import Restful.Types
+{-
+    Pause this.  This isn't the right place to multiplex
+-}
 
+{-
+import           Frontier.Class
+import qualified Frontier.PoliteStmFrontier as P -- Pass this in instead!
+import           Restful.Types
+
+import           Control.Concurrent.STM
 import           Control.Monad.IO.Class        (MonadIO, liftIO)
 import           Control.Monad.Trans.Reader    (ReaderT, ask, runReaderT)
 import           Data.Hashable
-import           Data.Vector ((!), Vector)
+import qualified Data.Heap as H
+import           Data.Heap                     (Heap)
+import           Data.Time.Clock.POSIX
+import           Data.Vector                   ((!), Vector)
 import qualified Data.Vector as V
 
-data Env = Env { numThreads :: !Int }
+data Env = Env { getNumThreads :: !Int
+               , getCrawlers   :: !(Vector P.PoliteStmFrontier)
+               , crawlerHeap   :: !(TVar (Heap Int))
+               }
 
 newtype MultiplexedFrontier a =
     MultiplexedFrontier { unMultiplexedFrontier :: ReaderT Env IO a }
@@ -22,23 +34,40 @@ newtype MultiplexedFrontier a =
 instance Frontier MultiplexedFrontier where
 
     completed :: Url -> UrlResult -> MultiplexedFrontier ()
-    completed = undefined
+    completed url result = do
+        Env nt cs _ <- MultiplexedFrontier ask
+        let h = hash url `mod` nt
+        liftIO $ P.signalUrlCompleted (cs ! h) url result
 
     insert :: Url -> MultiplexedFrontier ()
     insert url = do
-        
-        nt <- numThreads <$> MultiplexedFrontier ask
-        
+        Env nt cs _ <- MultiplexedFrontier ask
         let h = hash url `mod` nt
-
-        undefined
+        liftIO $ P.insertUrl (cs ! h) url
 
     nextUrl :: MultiplexedFrontier NextUrl
-    nextUrl = undefined
+    nextUrl = do
+        
+        Env nt cs tch <- MultiplexedFrontier ask
+
+        liftIO $ do
+
+            -- Choose crawler
+            atomically $ do
+                ch <- readTVar tch
+                undefined
+
+            -- Route request
+
+            now <- currentMillis
+            undefined -- popEarliestHostUrl
+
+currentMillis :: IO Millis
+currentMillis = Millis . round . (* 1000) <$> getPOSIXTime
 
 runMultiplexedFrontier :: Int -> MultiplexedFrontier a -> IO a
 runMultiplexedFrontier numThreads multiplexedFrontier = do
-
-    pCrawlers <- V.replicateM numThreads new
-
-    undefined
+    pCrawlers <- V.replicateM numThreads P.new
+    crawlerHeap <- newTVarIO $ H.fromList [0..numThreads-1]
+    runReaderT (unMultiplexedFrontier multiplexedFrontier) (Env numThreads pCrawlers crawlerHeap)
+-}
--- crawler2/src/Main.hs
+++ crawler2/src/Main.hs
@@ -2,15 +2,95 @@
 
 module Main ( main ) where
 
+import qualified Frontier.PoliteStmFrontier as P
+
+
+
 import Crawler        (runCrawler)
 import Frontier.Class (Frontier (..), Millis (..), NextUrl (..), UrlResult (..))
 import Restful.Class  (Restful (..))
-import Restful.Types  (mkUrl)
+import Restful.Types  (Url, mkUrl)
 import Scrape         (scrapeUrls)
 
-import Control.Concurrent     (threadDelay)
-import Control.Monad.IO.Class (MonadIO (liftIO))
+import Restful.IO (fetchGetImpl)
+import Control.Concurrent     --  (threadDelay)
+import Control.Concurrent.Async -- (forConcurrently_, replicateConcurrently)
+import Control.Monad
+import Control.Monad.IO.Class   (MonadIO (liftIO))
+import           Data.Time.Clock.POSIX
+import           Network.HTTP.Client           (Manager, defaultManagerSettings, newManager)
 
+import Data.Vector ((!), Vector)
+import qualified Data.Vector as V
+import Data.Hashable
+
+numThreads :: Int
+numThreads = 4
+
+main :: IO ()
+main = do
+
+    ps <- V.replicateM numThreads P.new
+
+    case mkUrl "http://127.0.0.1:3000" of
+        
+        Nothing -> error "Bad url"
+        
+        Just url -> do
+
+            addUrl ps url
+
+            js <- forM ps $ \p -> async $ do
+
+                http <- newManager defaultManagerSettings
+                
+                go http p ps
+
+            mapM_ wait js
+
+go http p ps = do
+
+    now <- currentMillis
+
+    P.popEarliestHostUrl p now >>= \case
+
+        NoMoreUrls -> do
+            threadDelay (250000)
+            putStrLn "No more urls.  Waited"
+            go http p ps
+
+        RetryIn (Millis ms) -> do
+            putStrLn ("Waiting " ++ show ms ++ "ms")
+            threadDelay (1000 * ms)
+            go http p ps
+
+        NextUrl url ->
+
+            fetchGetImpl http url >>= \case
+
+                Left _ ->
+                    P.signalUrlCompleted p url (UrlFailure (-1)) -- TODO code this
+
+                Right response -> do
+
+                    P.signalUrlCompleted p url Success -- TODO check
+
+                    let urls = scrapeUrls response
+                    mapM_ (addUrl ps) urls
+                    go http p ps
+
+
+currentMillis :: IO Millis
+currentMillis = Millis . round . (* 1000) <$> getPOSIXTime
+
+addUrl :: Vector P.PoliteStmFrontier -> Url -> IO ()
+addUrl ps url =
+    let h = hash url `mod` numThreads
+    in P.insertUrl (ps ! h) url
+
+
+
+{-
 main :: IO ()
 main = runCrawler simple3
 
@@ -53,3 +133,4 @@ go =
 
 lPutStrLn :: MonadIO m => String -> m ()
 lPutStrLn = liftIO . putStrLn
+-}
